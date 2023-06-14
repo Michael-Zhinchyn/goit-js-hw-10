72be13f821078620eb6e7c282f2f32e1a83b6ca5
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-06-14 03:49:24 +0000

    Deploying to gh-pages from @ Michael-Zhinchyn/goit-js-hw-10@3f08ce38f42e7c7595d7fc1c627bf472b89d6bec 🚀

---
+++ index.b04269b3.js
@@ -0,0 +1,2 @@
+document.querySelector(".breed-select");fetch("https://api.thecatapi.com/v1/breeds?api_key=live_sGqxpzYLvFus7p2nXND3T4qIBX7yjf1C275c2408M4sTblpioeOksmdnAZteZmPh").then((e=>console.log(e)));
+//# sourceMappingURL=index.b04269b3.js.map
