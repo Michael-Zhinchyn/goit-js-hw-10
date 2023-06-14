a8fccc5344bc16a969795f61038d8b3b6f1bd022
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-06-14 00:57:44 -0500

    update

---
+++ src/js/cat-api.js
@@ -0,0 +1,36 @@
+export function fetchBreeds() {
+  const BASE_URL = 'https://api.thecatapi.com/v1/';
+  const API_KEY =
+    'live_sGqxpzYLvFus7p2nXND3T4qIBX7yjf1C275c2408M4sTblpioeOksmdnAZteZmPh';
+
+  loader.style.display = 'block';
+
+  fetch(`${BASE_URL}breeds?api_key=${API_KEY}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then(data => {
+      selectInput.innerHTML = createMarkup(data);
+      loader.style.display = 'none';
+    })
+    .catch(error => {
+      console.log(error);
+      loader.style.display = 'none';
+    });
+}
+
+function createMarkup(items) {
+  return items
+    .map(item => `<option value="${item.id}">${item.name}</option>`)
+    .join('');
+}
+
+const selectInput = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+
+window.onload = function () {
+  loader.style.display = 'none';
+};
--- src/js/homework.js
+++ src/js/homework.js
@@ -1,13 +1,3 @@
-const selectBtn = document.querySelector('.breed-select');
-
-function fetchBreeds() {
-  const BASE_URL = 'https://api.thecatapi.com/v1/';
-  const API_KEY =
-    'live_sGqxpzYLvFus7p2nXND3T4qIBX7yjf1C275c2408M4sTblpioeOksmdnAZteZmPh';
-
-  fetch(`${BASE_URL}breeds?api_key=${API_KEY}`).then(response =>
-    console.log(response)
-  );
-}
+import { fetchBreeds } from './cat-api';
 
 fetchBreeds();
