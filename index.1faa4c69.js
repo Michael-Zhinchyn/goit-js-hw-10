26b2687fd5ca5ad14a45e98f058c12bbdf964711
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-06-16 22:35:37 +0000

    Deploying to gh-pages from @ Michael-Zhinchyn/goit-js-hw-10@aed25b47b0e2acbbb1f02b9645a6af8e6829131e 🚀

---
+++ index.1faa4c69.js
@@ -0,0 +1,2 @@
+document.getElementById("play-button").addEventListener("click",(function(){player.getPlayerState()===YT.PlayerState.PLAYING?player.pauseVideo():player.playVideo()})),document.getElementById("volume-slider").addEventListener("input",(function(){player.setVolume(this.value)}));
+//# sourceMappingURL=index.1faa4c69.js.map
