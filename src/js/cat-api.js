26b819137539b37fd791015d8877035c7b3caa2f
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-06-17 20:58:29 -0500

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -104,6 +104,6 @@ export function createCatInfo(catData, id) {
     <h2>${catBreed.name}</h2>
     <p>${catBreed.temperament}</p>
     <p>${catBreed.description}</p>
-    <img src="${cat.url}" alt="${catBreed.name}" width=500>
+    <img src="${cat.url}" alt="${catBreed.name}" width=800>
   `;
 }
