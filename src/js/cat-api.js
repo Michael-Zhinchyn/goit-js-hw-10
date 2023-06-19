1f435b8db6518ffb067606445cbfb5657c1136c9
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-06-19 09:29:11 -0500

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,20 +1,19 @@
 // Імпортуємо залежності
-import { Notify } from 'notiflix/build/notiflix-notify-aio'; // Бібліотека для показу сповіщень
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SlimSelect from 'slim-select';
 
 // Створюємо константи для наших HTML-елементів
 const selectBreed = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const errorMsg = document.querySelector('.error');
-const BASE_URL = 'https://api.thecatapi.com/v1/'; // Базовий URL нашого API
-const API_KEY = 'your_api_key'; // Ключ до нашого API (замініть "your_api_key" на ваш власний ключ)
+const BASE_URL = 'https://api.thecatapi.com/v1/';
+const API_KEY = 'your_api_key';
 const backdrop = document.querySelector('#backdrop');
 const catModal = document.querySelector('#cat-modal-content');
 const closeButton = document.querySelector('#close-button');
-export { loader };
 
 // Ініціалізація змінних
-let chosenBred;
+let chosenBred = null;
 let breeds = [];
 
 let slimSelect = new SlimSelect({
@@ -22,25 +21,22 @@ let slimSelect = new SlimSelect({
   placeholder: 'Select a breed',
   allowDeselect: true,
   deselectLabel: '<span class="placeholder">Select a breed</span>',
-  showFirstOption: false, // Додайте цю строку
+  showFirstOption: false,
   onChange: info => {
     let selectedBreed = info[0].value;
     if (selectedBreed) {
-      fetchCatByBreed(selectedBreed); // Передаємо selectedBreed як аргумент
+      fetchCatByBreed(selectedBreed);
     }
   },
 });
 
-// Приховуємо повідомлення про помилку та фоновий екран
 errorMsg.style.display = 'none';
 backdrop.style.display = 'none';
 
-// Додаємо слухач події для кнопки закриття
 closeButton.addEventListener('click', () => {
   backdrop.style.display = 'none';
 });
 
-// Функція для створення HTML-розмітки для порід
 function createBreedsMarkup(items) {
   slimSelect.setData(
     [{ text: '', value: '' }].concat(
@@ -51,74 +47,68 @@ function createBreedsMarkup(items) {
   );
 }
 
-// Функція для отримання даних про породи котів з API
 export function fetchBreeds() {
-  loader.style.display = 'block'; // Показуємо "завантажувач"
+  loader.style.display = 'block';
 
-  // Здійснюємо запит до API
   fetch(`${BASE_URL}breeds?api_key=${API_KEY}`)
     .then(response => {
       if (!response.ok) {
-        Notify.failure(`${errorMsg.textContent}`); // Показуємо повідомлення про помилку, якщо виникла проблема
+        Notify.failure(`${errorMsg.textContent}`);
       }
-      return response.json(); // Повертаємо дані в форматі JSON
+      return response.json();
     })
     .then(data => {
-      breeds = data; // Зберігаємо дані про породи в змінну
-      selectBreed.innerHTML = createBreedsMarkup(data); // Заповнюємо наш випадаючий список даними про породи
-
-      loader.style.display = 'none'; // Приховуємо "завантажувач"
+      breeds = data;
+      selectBreed.innerHTML = createBreedsMarkup(data);
+      loader.style.display = 'none';
     })
     .catch(error => {
-      console.log(error); // Показуємо помилку в консолі
-
-      loader.style.display = 'none'; // Приховуємо "завантажувач"
+      console.log(error);
+      loader.style.display = 'none';
     });
 }
 
-// Функція для отримання котика обраної породи
-export function fetchCatByBreed() {
-  loader.style.display = 'block'; // Показуємо "завантажувач"
+export function fetchCatByBreed(breed) {
+  if (!breed) {
+    return;
+  }
 
-  // Здійснюємо запит до API
-  fetch(`${BASE_URL}images/search?breed_ids=${chosenBred}`)
+  loader.style.display = 'block';
+
+  fetch(`${BASE_URL}images/search?breed_ids=${breed}`)
     .then(response => {
       if (!response.ok) {
-        Notify.failure('Котик не знайдений'); // Показуємо повідомлення про помилку, якщо котик не знайдений
+        Notify.failure('Котик не знайдений');
       }
-      return response.json(); // Повертаємо дані в форматі JSON
+      return response.json();
     })
     .then(data => {
-      catModal.innerHTML = createCatInfo(data, chosenBred); // Заповнюємо модальне вікно даними про котика
-      backdrop.style.display = 'flex'; // Показуємо модальне вікно
-      loader.style.display = 'none'; // Приховуємо "завантажувач"
+      catModal.innerHTML = createCatInfo(data, breed);
+      backdrop.style.display = 'flex';
+      loader.style.display = 'none';
     })
     .catch(error => {
-      console.log(error); // Показуємо помилку в консолі
-      loader.style.display = 'none'; // Приховуємо "завантажувач"
+      Notify.failure("this cat wasn't found :(", 'okay');
+      console.log(error);
+      loader.style.display = 'none';
     });
 }
 
-// Додаємо слухач події для зміни вибраної породи в списку
-selectBreed.addEventListener('change', onChange);
+selectBreed.addEventListener('change', event => {
+  chosenBred = event.target.value;
+  if (chosenBred) {
+    fetchCatByBreed(chosenBred);
+  }
+});
 
-// Функція, яка викликається при зміні вибраної породи
-export function onChange(event) {
-  chosenBred = event.target.value; // Зберігаємо обрану породу
-  fetchCatByBreed(); // Отримуємо котика обраної породи
+function getBreedById(id) {
+  return breeds.find(breed => breed.id === id);
 }
 
-// Функція для отримання даних про породу по її ID
-export function getBreedById(id) {
-  return breeds.find(breed => breed.id === id); // Повертаємо породу з відповідним ID
-}
+function createCatInfo(catData, id) {
+  const cat = catData[0];
+  const catBreed = getBreedById(id);
 
-// Функція для створення HTML-розмітки для котика
-export function createCatInfo(catData, id) {
-  const cat = catData[0]; // Дані про котика
-  const catBreed = getBreedById(id); // Дані про породу котика
-
-  // Повертаємо HTML-розмітку з даними про котика
   return `
     <h2>${catBreed.name}</h2>
     <p>${catBreed.temperament}</p>
